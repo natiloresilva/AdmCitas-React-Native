a51c1704708c482d249c0706cf4d4e26856bc578
Natalia <natalia.pupushi@gmail.com>
2021-02-16 10:01:24 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, FlatList} from 'react-native';
+import Cita from './components/Cita';
+import Formulario from './components/Formulario';
+
+const App = () => {
+  //Definimos el state de citas
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Lara', propietario: 'Oscar', sintomas: 'Duerme mucho'},
+    {
+      id: '2',
+      paciente: 'Kevin',
+      propietario: 'Julieta',
+      sintomas: 'Duerme mucho',
+    },
+    {
+      id: '3',
+      paciente: 'Galileo',
+      propietario: 'Natalia',
+      sintomas: 'Duerme mucho',
+    },
+  ]);
+
+  //Elimina los pacientes del state
+  const eliminarPaciente = (id) => {
+    setCitas((citasActuales) => {
+      return citasActuales.filter((cita) => cita.id !== id);
+    });
+  };
+  return (
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Administrador de citas</Text>
+
+      <Formulario />
+
+      <Text style={styles.titulo}>
+        {citas.length > 0
+          ? 'Gestiona tus pacientes'
+          : 'No hay citas, agrega una'}
+      </Text>
+
+      <FlatList
+        data={citas}
+        renderItem={({item}) => (
+          <Cita item={item} eliminarPaciente={eliminarPaciente} />
+        )}
+        keyExtractor={(cita) => cita.id}
+      />
+
+      {/* {citas.map(cita => (<View>
+        <Text key={id}>{cita.paciente}</Text>
+     </View>))} */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#A2D5E1',
+    flex: 1,
+  },
+  titulo: {
+    color: '#2c4551',
+    textAlign: 'center',
+    marginTop: 40,
+    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default App;
---
+++ components/Formulario.js
@@ -0,0 +1,125 @@
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+const Formulario = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+
+  //Muestra u oculta el Date Picker
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const confirmarFecha = (date) => {
+    console.warn('A date has been picked: ', date);
+    hideDatePicker();
+  };
+
+  //Muestra u oculta el Time Picker
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+
+  const confirmarHora = (date) => {
+    console.warn('A date has been picked: ', date);
+    hideTimePicker();
+  };
+
+  return (
+    <>
+      <View style={styles.formulario}>
+        <View>
+          <Text style={styles.label}>Paciente:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(texto) => console.log(texto)}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Dueño:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(texto) => console.log(texto)}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Teléfono de contacto:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(texto) => console.log(texto)}
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View>
+          <Button title="Seleccionar fecha" onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={confirmarFecha}
+            onCancel={hideDatePicker}
+            locale="es_ES"
+            headerTextIOS="Elige la fecha"
+          />
+        </View>
+
+        <View>
+          <Button title="Seleccionar hora" onPress={showTimePicker} />
+          <DateTimePickerModal
+            isVisible={isTimePickerVisible}
+            mode="time"
+            onConfirm={confirmarHora}
+            onCancel={hideTimePicker}
+            locale="es_ES"
+            headerTextIOS="Elige una hora"
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Síntomas:</Text>
+          <TextInput
+            multiline
+            style={styles.input}
+            onChangeText={(texto) => console.log(texto)}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  formulario: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: '2.5%',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 20,
+  },
+
+  input: {
+    marginTop: 10,
+    height: 50,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+});
+
+export default Formulario;
